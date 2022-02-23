2e5094f3dd679bcbbe232050763ba5cf1be12343
miyeaier <miyesier@gmail.com>
2022-02-23 11:02:48 +0100

    add NEVBAR and placeholder on header and add styling use react-bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,39 @@
-import {Container,Navbar } from 'react-bootstrap'
+import {
+  Container,
+  FormControl,
+  Navbar,
+  Badge,
+  Nav,
+  Dropdown,
+} from "react-bootstrap";
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
-          <a href="/">Shopping Cart</a>
+          <a href="/">Shopping Cart </a>
         </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            placeholder="Search a product"
+            className="m-auto"
+          />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown alignRight>
+            <Dropdown.Toggle variant="success">
+              <Badge>{10}</Badge>
+            </Dropdown.Toggle>
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <span style={{ padding: 10 }}>Cart is Empty</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
       </Container>
     </Navbar>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
